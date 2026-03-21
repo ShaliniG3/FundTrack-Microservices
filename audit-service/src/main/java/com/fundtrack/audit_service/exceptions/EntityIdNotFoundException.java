4a7d6e6bf98cf4e2d96782b8b830b8b48924bb12
ShaliniG3 <Shalini.G3@cognizant.com>
2026-03-21 12:00:09 +0530

    Add files via upload

---
+++ audit-service/src/main/java/com/fundtrack/audit_service/exceptions/EntityIdNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fundtrack.audit_service.exceptions;
+
+
+public class EntityIdNotFoundException extends RuntimeException {
+    public EntityIdNotFoundException(String message) {
+        super(message);
+    }}
---
+++ audit-service/src/main/java/com/fundtrack/audit_service/model/EntityType.java
@@ -0,0 +1,17 @@
+package com.fundtrack.audit_service.model;
+
+/**
+ * Categorizes the types of entities within the FundTrack system for auditing purposes.
+ */
+public enum EntityType {
+    PROGRAM,
+    ELIGIBILITY_RULE,
+    REQUIRED_DOCUMENT,
+    USER,
+    PAYMENT,       // Added for financial tracking
+    DISBURSEMENT,// Added for installment tracking
+    APPLICATION,
+    AUDIT_LOG
+}
+
+
